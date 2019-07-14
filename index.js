d3f6ce342f06004702c8c78e050c0349b06da264
Rannie Ollit <einnar82@gmail.com>
2019-07-14 13:47:33 +0800

    Added the port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 const apicache = require("apicache");
 const cache = apicache.middleware;
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(cache("5 minutes"));
